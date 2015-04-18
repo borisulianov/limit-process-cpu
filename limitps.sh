8c6d639a88c57f8d2f508b2e700835afdc83a694
Ariel Sebastián Becker <arielsbecker@users.noreply.github.com>
2015-04-18 18:54:01 -0300

    Actualizo limitps.sh
    Agrego el chequeo necesario para saber si cpulimit está instalado en el sistema.

--- limitps.sh
+++ limitps.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
-echo -n "Proceso? > "
-read NombreProceso
-proceso="$(pidof -s $NombreProceso)"
+which cpulimit > /dev/null
 status="$?"
 
 if [ $status -eq 0 ]
-  then
-    echo -n "Límite CPU? >"
-    read LimiteCPU
-    echo "Limitando $NombreProceso a $LimiteCPU % de tiempo de CPU."
-    cpulimit --pid=$proceso -l $LimiteCPU > /dev/null
-  else
-    echo "No se han encontrado instancias del proceso $NombreProceso."
+	then
+	echo -n "Proceso? > "
+	read NombreProceso
+	proceso="$(pidof -s $NombreProceso)"
+	status="$?"
+
+	if [ $status -eq 0 ]
+	  then
+		echo -n "Límite CPU? >"
+		read LimiteCPU
+		echo "Limitando $NombreProceso a $LimiteCPU % de tiempo de CPU."
+		cpulimit --pid=$proceso -l $LimiteCPU > /dev/null
+	  else
+		echo "No se han encontrado instancias del proceso $NombreProceso."
+	fi
+else
+  echo "Al parecer la aplicación cpulimit no está instalada."
+  echo "Para instalarla ejecute:"
+  echo "    sudo apt-get install cpulimit"
 fi
